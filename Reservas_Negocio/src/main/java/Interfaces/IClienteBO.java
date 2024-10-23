1a2fb5b8674fc579c5e6422c32fdfe4b121e094a
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-10-23 01:47:39 -0700

    asdasdasd
    asdasdasd

--- Reservas_Negocio/src/main/java/Interfaces/IClienteBO.java
+++ Reservas_Negocio/src/main/java/Interfaces/IClienteBO.java
@@ -4,11 +4,39 @@
  */
 package Interfaces;
 
+import DTO.ClienteDTO;
+import java.util.List;
+
 /**
- *
+ * Interfaz que define las operaciones básicas para la gestión de clientes en la capa de negocio (BO).
+ * Esta interfaz proporciona un contrato para las implementaciones que manejarán la persistencia
+ * y recuperación de información de clientes.
+ * 
  * @author skevi
  */
 public interface IClienteBO {
+    
+    /**
+     * Agrega un nuevo cliente a la base de datos.
+     * 
+     * @param cliente El objeto {@link Cliente} que se desea agregar.
+     */
+    public void agregarCliente(ClienteDTO cliente);
+    
+    /**
+     * Obtiene los detalles de un cliente específico a partir de su objeto.
+     * 
+     * @param id id del cliente.
+     * @return Cliente.
+     */
+    public ClienteDTO obtenerCliente(Long id);
+    
+    /**
+     * Recupera una lista de todos los clientes almacenados en la base de datos.
+     * 
+     * @return Una lista de objetos {@link Cliente} que representa todos los clientes.
+     */
+    public List<ClienteDTO> obtenerClientes();
     
 }
  
