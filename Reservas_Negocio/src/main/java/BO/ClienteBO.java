ba12a51b304f738bd37a83ec9e8c2005f555312b
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-10-24 14:07:11 -0700

    asdasdasdasd
    asdasdasdasd

--- Reservas_Negocio/src/main/java/BO/ClienteBO.java
+++ Reservas_Negocio/src/main/java/BO/ClienteBO.java
@@ -3,10 +3,16 @@ package BO;
 import Convertidores.ClienteCVR;
 import DAO.ClienteDAO;
 import DTO.ClienteDTO;
+import Entidades.Cliente;
 import Excepciones.BOException;
+import Excepciones.ConversionException;
+import Excepciones.DAOException;
 import Interfaces.IClienteBO;
 import Interfaces.IClienteDAO;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Clase que maneja la lógica de negocio para los clientes.
@@ -15,23 +21,75 @@ import java.util.List;
  * @author sebastian
  */
 public class ClienteBO implements IClienteBO {
+
+    // instancia del logger para depuracion
+    private static final Logger LOG = Logger.
+            getLogger(ClienteBO.class.getName());
     
     private final IClienteDAO clienteDAO; // instancia de interfaz clienteDAO
-    private final ClienteCVR clienteCVR;
+    private final ClienteCVR clienteCVR; // instancia del convertidor de cliente
 
+    /**
+     * Constructor por defecto de la clase.
+     */
     public ClienteBO() {
         this.clienteDAO = new ClienteDAO();
         this.clienteCVR = new ClienteCVR();
     }
 
+    /**
+     * Metodo de control entre la capa de DAO y BO para el metodo de obtener
+     * cliente por id, este metodo se encarga de obtener tal cliente de 
+     * la capa de DAO y lo convierte a un DTO para su uso en capas superiores.
+     * 
+     * @param id id del cliente a buscar
+     * @return retorna un ClienteDTO
+     * @throws BOException En caso de excepciones en capas inferiores.
+     */
     @Override
     public ClienteDTO obtenerCliente(Long id) throws BOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            Cliente cliente = clienteDAO.obtenerCliente(id);
+            ClienteDTO clienteDTO = clienteCVR.toDTO(cliente);
+            
+            LOG.log(Level.INFO, "exito al obtener al cliente por id en BO");
+            
+            return clienteDTO;
+        }
+        catch(DAOException de){
+            LOG.log(Level.SEVERE, "Error al obtener el cliente por id en BO", 
+                    de);
+            throw new BOException(de.getMessage());
+        }
+        catch(ConversionException ce){
+            LOG.log(Level.SEVERE, "Error al obtener el cliente por id en BO", 
+                    ce);
+            throw new BOException("Error al obtener el cliente por id");
+        }
     }
 
     @Override
     public List<ClienteDTO> obtenerClientes() throws BOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       try{
+           List<Cliente> entidades = clienteDAO.obtenerClientes();
+           List<ClienteDTO> dto = new ArrayList<>();
+           
+           for (ClienteDTO clienteDTO : dto) {
+               entidades.add(clienteCVR.toEntity(clienteDTO));
+           }
+           
+           return dto;
+       }
+       catch(DAOException de){
+            LOG.log(Level.SEVERE, "Error al obtener los clientes en BO", 
+                    de);
+            throw new BOException(de.getMessage());
+        }
+        catch(ConversionException ce){
+            LOG.log(Level.SEVERE, "Error al obtener el clientes en BO", 
+                    ce);
+            throw new BOException("Error al obtener el cliente por id");
+        }
     }
     
 }
--- Reservas_Negocio/src/main/java/BO/MesaBO.java
+++ Reservas_Negocio/src/main/java/BO/MesaBO.java
@@ -3,22 +3,32 @@ package BO;
 import Convertidores.MesaCVR;
 import DAO.MesaDAO;
 import DTO.MesaDTO;
+import Entidades.Mesa;
 import Excepciones.BOException;
+import Excepciones.ConversionException;
+import Excepciones.DAOException;
 import Interfaces.IMesaBO;
 import Interfaces.IMesaDAO;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * 
  * @author skevi
  */
 public class MesaBO implements IMesaBO {
+
+    private static final Logger LOG = Logger.
+            getLogger(MesaBO.class.getName());
     
-    private final IMesaDAO mesaDAO;
-    private final MesaCVR mesaCVR;
+    private final IMesaDAO mesaDAO; //instancia de la interfaz de MesaDAO
+    private final MesaCVR mesaCVR; // instnacia del convertidor de mesa
 
     /**
-     * 
+     * Constructor por defecto de la clase que inicializa los atributos 
+     * del convertidor y del acceso al DAO.
      */
     public MesaBO() {
         this.mesaDAO = new MesaDAO();   
@@ -27,22 +37,80 @@ public class MesaBO implements IMesaBO {
 
     @Override
     public void agregarMesas(List<MesaDTO> mesas) throws BOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+           List<Mesa> entidades = new ArrayList<>();
+           
+            for (MesaDTO mesaDTO : mesas) {
+                entidades.add(mesaCVR.toEntity(mesaDTO));
+            }
+           
+            mesaDAO.agregarMesas(entidades);
+        }
+        catch(DAOException de){
+            LOG.log(Level.SEVERE, "Error al agregar las mesas en BO");
+            throw new BOException(de.getMessage());
+        } catch (ConversionException ex) {
+            Logger.getLogger(MesaBO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new BOException("Error al agregar las mesas en BO");
+        }
+      
     }
 
     @Override
     public List<MesaDTO> consultarMesas() throws BOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+           List<Mesa> entidades = mesaDAO.consultarMesas();
+           List<MesaDTO> dto = new ArrayList<>();
+           
+            for (Mesa mesa : entidades) {
+                dto.add(mesaCVR.toDTO(mesa));
+            }
+           
+            return dto;
+        }
+        catch(DAOException de){
+            LOG.log(Level.SEVERE, "Error al consultar las mesas en BO");
+            throw new BOException(de.getMessage());
+        } catch (ConversionException ex) {
+            Logger.getLogger(MesaBO.class.getName()).
+                    log(Level.SEVERE, null, ex);
+            throw new BOException("Error al consultar las mesas");
+        }
     }
 
     @Override
     public int cantidadMesasPorTipo(String tipo) throws BOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            return mesaDAO.cantidadMesasPorTipo(tipo);
+        }
+        catch(DAOException de){
+            LOG.log(Level.SEVERE, "Error al consultar la cantidad de mesas "
+                    + "por tipo en BO");
+            throw new BOException(de.getMessage());
+        }
     }
 
     @Override
     public List<MesaDTO> obtenerMesasPorTipo(String tipo) throws BOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+           List<Mesa> entidades = mesaDAO.obtenerMesasPorTipo(tipo);
+           List<MesaDTO> dto = new ArrayList<>();
+           
+            for (Mesa mesa : entidades) {
+                dto.add(mesaCVR.toDTO(mesa));
+            }
+           
+            return dto;
+        }
+        catch(DAOException de){
+            LOG.log(Level.SEVERE, "Error al consultar las mesas por "
+                    + "tipo en BO");
+            throw new BOException(de.getMessage());
+        } catch (ConversionException ex) {
+            Logger.getLogger(MesaBO.class.getName()).
+                    log(Level.SEVERE, null, ex);
+            throw new BOException("Error al consultar las mesas");
+        }
     }
 
 }
